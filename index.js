febb9062f4e04f250657e1e4da1f1e88c301076e
Hannah Lormenyo <lormenyoh@gmail.com>
2024-06-19 07:43:45 +0100

    added fare calculator endpoint

--- index.js
+++ index.js
@@ -30,6 +30,24 @@ app.get("/stop/:stopAbv", async (req, res) => {
 });
 
 // TODO: get fares
+app.get("/fareCalculator", async (req, res) => {
+    try {
+        const from = req.query.from;
+        const to = req.query.to;
+        const adults = req.query.adults;
+        const children = req.query.children;
+
+        const URL = `http://luasforecasts.rpa.ie/xml/get.ashx?action=farecalc&from=${from}&to=${to}&adults=${adults}&children=${children}&encrypt=false`;
+        const response = await axios.get(URL);
+        const data = JSON.parse(toJson(response.data));
+     
+        res.status(200).send(data);
+    } catch (error) {
+        console.log(error);
+        res.status(404).send(error.response.data);
+    }
+   
+});
 
 // TODO: get news
 
