1181335866bb326fb618e28fc9362157ce8d54a3
Hannah Lormenyo <lormenyoh@gmail.com>
2024-06-27 23:29:37 +0100

    added stop locations

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 dotenv.config();
 
 const port = process.env.PORT;
+const BASE_URL = "http://luasforecasts.rpa.ie/xml/get.ashx";
 
 
 app.get("/", (req, res)=>{
@@ -17,7 +18,7 @@ app.get("/", (req, res)=>{
 app.get("/stop/:stopAbv", async (req, res) => {
     try {
         const stopAbv = req.params.stopAbv;
-        const URL = `http://luasforecasts.rpa.ie/xml/get.ashx?action=forecast&stop=${stopAbv}&encrypt=false`;
+        const URL = `${BASE_URL}?action=forecast&stop=${stopAbv}&encrypt=false`;
         const response = await axios.get(URL);
         const data = JSON.parse(toJson(response.data));
      
@@ -37,7 +38,22 @@ app.get("/fareCalculator", async (req, res) => {
         const adults = req.query.adults;
         const children = req.query.children;
 
-        const URL = `http://luasforecasts.rpa.ie/xml/get.ashx?action=farecalc&from=${from}&to=${to}&adults=${adults}&children=${children}&encrypt=false`;
+        const URL = `${BASE_URL}?action=farecalc&from=${from}&to=${to}&adults=${adults}&children=${children}&encrypt=false`;
+        const response = await axios.get(URL);
+        const data = JSON.parse(toJson(response.data));
+     
+        res.status(200).send(data);
+    } catch (error) {
+        console.log(error);
+        res.status(404).send(error.response.data);
+    }
+   
+});
+
+// Stop Locations
+app.get("/stopLocations", async (req, res) => {
+    try {
+        const URL = `${BASE_URL}?action=stops&encrypt=false`;
         const response = await axios.get(URL);
         const data = JSON.parse(toJson(response.data));
      
